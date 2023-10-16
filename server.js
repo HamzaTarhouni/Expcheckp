92dbefe8bcf4130bff46fbb3f4a50544a8b75290
HamzaTarhouni <tarhounihamza51@gmail.com>
2023-10-16 14:18:32 +0100

    save3

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // Dependencies
-const express = require('express');
+const express = require('express');  
 const path = require('path');
+
 
 // Initialize the application
 const app = express();
 
 // Define a port number
-const port = 5000;
+const port = 5000;              
 
 // Define authorize middleware
 const authorize = (req, res, next) => {
-  const isAuth = true;
+  const isAuth = true; // You can implement your authentication logic here.
   if (isAuth) {
     next();
   } else {
@@ -24,25 +25,23 @@ const requestTime = (req, res, next) => {
   const day = date.getDay();
   const hours = date.getHours();
 
-  if (day < 1 || day < 5 || hours < 9 || hours > 17) {
+  if (day < 1 || day > 5 || hours < 9 || hours > 17) { // Corrected the condition
     return res.status(401).send('We are closed');
   }
 
   next();
 };
-// Serve the Home.html file as the default route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Home.html'));
-  });
 
 // Middleware use
 app.use(requestTime);
 app.use(authorize);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Home.html'));
+});
 
-// Serve static files (CSS, JavaScript, etc.)
-app.use(express.static(__dirname));
 
 // Start the server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
